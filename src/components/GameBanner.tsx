ef33009658597a9b78c2f38d684d733ddb763cb3
nerdstarcode <sthivendev@gmail.com>
2022-09-16 21:03:46 -0300

    terminado!!!!

--- src/components/GameBanner.tsx
+++ src/components/GameBanner.tsx
@@ -2,14 +2,14 @@ interface GameBannerProps{
     bannerUrl: string;
     title: string;
     adsCount: number;
+    bannerNumber: number;
 }
-
 
 export function GameBanner(props: GameBannerProps){
     return(
-        <a href="" className='relative rounded-lg overflow-hidden'>
+        <a href="#" className={`relative rounded-lg overflow-hidden keen-slider__slide number-slide${props.bannerNumber}`}>
             <img src={props.bannerUrl} alt="" />
-            <div className="w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0">
+            <div className={`w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0 `}>
                 <strong className="font-bold text-white block">{props.title}</strong>
                 <span className="text-zinc-300 text-sm block mt-1">{props.adsCount} anúncio(s)</span>
             </div>
